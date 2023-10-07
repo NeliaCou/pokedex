0db0668c76f803015b6bac49fbf24991f05034c2
Nelia <nelia.courtais@live.fr>
2023-10-07 22:35:08 +0200

    second composit

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,33 @@
+import React from "react";
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
-  return (
-    <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="Bulbasaur"
-      />
-      <figcaption>Bulbasaur</figcaption>
-    </figure>
-  );
+  const pokemon = pokemonList[0];
+  if (pokemon.imgSrc) {
+    return (
+      <figure>
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    );
+  } else {
+    return (
+      <figure>
+        <p>???</p>
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    );
+  }
 }
 
 export default PokemonCard;
