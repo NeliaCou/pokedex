21e0c70d6d0d9f3ab9760b1f40b5d531c6013b1b
Nelia <nelia.courtais@live.fr>
2023-10-07 21:35:09 +0200

    changed result

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,6 +1,4 @@
-import PokemonCard from "./components/PokemonCard";
-
-function App() {
+function PokemonCard() {
   return (
     <figure>
       <img
@@ -12,4 +10,4 @@ function App() {
   );
 }
 
-export default App;
+export default PokemonCard;
