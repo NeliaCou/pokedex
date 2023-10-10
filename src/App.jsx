d94895dd0e9752f5cc9c2539d11b30545dc5cb72
Nelia <nelia.courtais@live.fr>
2023-10-10 13:17:38 +0200

    add buttons to change pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
 import PokemonCard from "./components/PokemonCard";
+import { useState } from "react";
 
 function App() {
+  const [pokemonIndex, setpokemonIndex] = useState(0);
+
+  const handleClick = () => {
+    if (pokemonIndex < pokemonList.length - 1) {
+      setpokemonIndex(pokemonIndex + 1);
+    }
+  };
+
+  const handleClickBis = () => {
+    if (pokemonIndex > 0) {
+      setpokemonIndex(pokemonIndex - 1);
+    }
+  };
+
   const pokemonList = [
     {
       name: "bulbasaur",
@@ -8,17 +23,35 @@ function App() {
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
       name: "mew",
     },
   ];
 
-  const selectedPokemon = pokemonList[0];
+  const selectedPokemon = pokemonList[pokemonIndex];
 
   return (
     <div>
-      <PokemonCard pokemon = {selectedPokemon} />
+      <PokemonCard pokemon={selectedPokemon} />
+      <button onClick={handleClickBis}>Précédent</button>
+      <button onClick={handleClick}>Suivant</button>
     </div>
   );
 }
 
 export default App;
+
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -28,16 +28,3 @@ PokemonCard.propTypes = {
 }
 
 export default PokemonCard;
-
-//Définir le type des props : dans ton composant PokemonCard, 
-// utilise le package prop-types pour définir le type des props. 
-// Tu dois fournir une définition complète de la forme pour la prop pokemon.
-
-// Pour rappel, pokemon est un objet avec la forme suivante :
-
-// {
-//   name: "...",
-//   imgSrc: "...",
-// }
-// Son "prop type" est donc une shape (le type pour les objets), avec une propriété name (requise) 
-// et une propriété imgSrc (optionnelle).
