5fb442bee92f5ccad1662ec59c21fd811be64330
Nelia <nelia.courtais@live.fr>
2023-10-10 09:47:31 +0200

    first prop-type added

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,5 +1,6 @@
-function PokemonCard(props) {
-  const {pokemon} = props;
+import PropTypes from "prop-types";
+
+function PokemonCard({pokemon}) {
   console.log(pokemon.name);
 
   if (pokemon.name && pokemon.imgSrc) {
@@ -19,4 +20,24 @@ function PokemonCard(props) {
   }
 }
 
+PokemonCard.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imgSrc: PropTypes.string.isRequired,
+  }).isRequired,
+}
+
 export default PokemonCard;
+
+//Définir le type des props : dans ton composant PokemonCard, 
+// utilise le package prop-types pour définir le type des props. 
+// Tu dois fournir une définition complète de la forme pour la prop pokemon.
+
+// Pour rappel, pokemon est un objet avec la forme suivante :
+
+// {
+//   name: "...",
+//   imgSrc: "...",
+// }
+// Son "prop type" est donc une shape (le type pour les objets), avec une propriété name (requise) 
+// et une propriété imgSrc (optionnelle).
