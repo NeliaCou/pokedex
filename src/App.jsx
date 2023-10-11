9efc1c65a0ed150d144e0677fcc54eec319de1cd
Nelia <nelia.courtais@live.fr>
2023-10-11 14:04:24 +0200

    tests

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,36 @@ import NavBar from "./components/NavBar";
 import { uuidGenerator } from "./components/UuidGenerator";
 
 function App() {
-  const [pokemonId, setPokemonId] = useState(null);
+
+const [pokemonId, setPokemonId] = useState(null);
 
   const pokemonList = [
     {
-      id: uuidGenerator,
+      id: uuidGenerator(),
       name: "bulbasaur",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
-      id: uuidGenerator,
+      id: uuidGenerator(),
       name: "charmander",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
     },
     {
-      id: uuidGenerator,
+      id: uuidGenerator(),
       name: "squirtle",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
     },
     {
-      id: uuidGenerator,
+      id: uuidGenerator(),
       name: "pikachu",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
     },
     {
-      id: uuidGenerator,
+      id: uuidGenerator(),
       name: "mew",
     },
   ];
