62e1517dcc7cae6eb5f1b2813960635cadb1fc36
Nelia <nelia.courtais@live.fr>
2023-10-13 12:36:12 +0200

    need to fix bugs

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,12 @@
 import PropTypes from "prop-types";
 
-
 function NavBar({ pokemonList, handleClick }) {
-
-
-
-  
   return (
     <>
       {pokemonList.map((pokemon) => (
         <button key={pokemon.id} onClick={() => handleClick(pokemon.id)}>
           {pokemon.name}
+          {pokemon.name === "pikachu" ? alert("pika pikachu !!!") : null}
         </button>
       ))}
     </>
