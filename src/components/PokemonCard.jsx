996a6a8f37b739464dbe345782b67c1bd031594f
Nelia <nelia.courtais@live.fr>
2023-10-13 11:33:17 +0200

    add opérateur ternaire

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -11,21 +11,24 @@ const pokemonList = [
 
 function PokemonCard() {
   const pokemon = pokemonList[0];
-  if (pokemon.imgSrc) {
-    return (
-      <figure>
-        <img src={pokemon.imgSrc} alt={pokemon.name} />
-        <figcaption>{pokemon.name}</figcaption>
-      </figure>
-    );
-  } else {
-    return (
-      <figure>
-        <p>???</p>
-        <figcaption>{pokemon.name}</figcaption>
-      </figure>
-    );
-  }
+  
+  return pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p> ;
+  
+  // if (pokemon.imgSrc) {
+  //    (
+  //     <figure>
+  //       <img src={pokemon.imgSrc} alt={pokemon.name} />
+  //       <figcaption>{pokemon.name}</figcaption>
+  //     </figure>
+  //   );
+  // } else {
+  //   return (
+  //     <figure>
+  //       <p>???</p>
+  //       <figcaption>{pokemon.name}</figcaption>
+  //     </figure>
+  //   );
+  // }
 }
 
 export default PokemonCard;
